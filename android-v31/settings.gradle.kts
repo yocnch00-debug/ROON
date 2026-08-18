6c909b4223925d93d472c9093ec409ff47723d56
car audio <yocnch00@gmail.com>
2026-08-18 20:18:42 +0900

    Add v31 Android settings

---
+++ android-v31/settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "ONRoonLinkR8v31"
+include(":app")
