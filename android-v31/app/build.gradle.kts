2f8792bee84baafeae2622718dcee051401998ee
car audio <yocnch00@gmail.com>
2026-08-19 03:42:45 +0900

    Bump R8 policy app to v31.1

--- android-v31/app/build.gradle.kts
+++ android-v31/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.onroonlink.r8v31"
         minSdk = 26
         targetSdk = 29
-        versionCode = 31
-        versionName = "1.0-r8-netshare-policy-v31"
+        versionCode = 311
+        versionName = "1.0-r8-netshare-policy-v31.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
