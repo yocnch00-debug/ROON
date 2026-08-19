eac2485db9275576c7618d7b239305e7d5600426
car audio <yocnch00@gmail.com>
2026-08-19 18:34:04 +0900

    Bump S26 gateway VPN-auto version

--- android-s26-gateway/app/build.gradle.kts
+++ android-s26-gateway/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.onroonlink.s26gateway"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0-gateway"
+        versionCode = 2
+        versionName = "1.1-phone-vpn-auto"
     }
 
     signingConfigs {
