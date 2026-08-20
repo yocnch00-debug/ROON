bb2f404eaa123af8c3ae05439e435e83422ad070
car audio <yocnch00@gmail.com>
2026-08-20 15:55:18 +0900

    Add unified app Gradle config

---
+++ s26-alpha6-unified/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.onroonlink.s26unified"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.onroonlink.s26unified"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0-alpha6-transport21"
+    }
+}
