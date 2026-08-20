82ec5eaf19eae0b6051a1f728f7f30f7ac24a041
car audio <yocnch00@gmail.com>
2026-08-20 15:54:47 +0900

    Add S26 alpha6 unified project settings

---
+++ s26-alpha6-unified/settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "S26Alpha6Unified"
+include(":app")
